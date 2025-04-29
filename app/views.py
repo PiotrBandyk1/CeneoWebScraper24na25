f675e66273a906cad6a7ca48a306a92385490396
PiotrBandyk1 <15004@eans-nt.edu.pl>
2025-04-29 10:24:39 +0200

    com

--- app/views.py
+++ app/views.py
@@ -1,12 +1,16 @@
 from app import app
-from flask import render_template
+from flask import render_template, request, redirect, url_for
 
 @app.route('/')
 def index():
     return render_template("index.html")
 
-@app.route('/extract')
+@app.route('/extract', methods=["GET", "POST"])
 def extract():
+    if request.method == "POST":
+        product_id = request.form.get("product_id")
+        if product_id and product_id.isdigit():
+            return redirect(url_for("product", product_id=int(product_id)))
     return render_template("extract.html")
 
 @app.route('/products')
@@ -18,6 +22,6 @@ def author():
     return render_template("author.html")
 
 @app.route('/product/<int:product_id>')
-def product(product_id:int):
+def product(product_id: int):
     return render_template("product.html", product_id=product_id)
 
